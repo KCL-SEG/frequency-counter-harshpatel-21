c3f541adf2dbb48dc4cb539d3c9bff92ebf972c7
Harsh <78853872+harshpatel-21@users.noreply.github.com>
2023-09-26 14:05:50 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -5,7 +5,7 @@ from collections import Counter
 
 def frequencies(items):
     frequencies = {}
-    for item in items:
-        frequencies.setdefault(str(item),0)
-        frequencies[str(item)] += 1
+    for item in map(str,items):
+        frequencies.setdefault(item,0)
+        frequencies[item] += 1
     return frequencies
